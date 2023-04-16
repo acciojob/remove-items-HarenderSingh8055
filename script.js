003b4976a90088af9e3d07cf871887a2f936f45e
HarenderSingh8055 <124910145+HarenderSingh8055@users.noreply.github.com>
2023-04-16 12:56:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+let button = document.querySelector("[type=button]");
+let color = document.querySelector("#colorSelect");
+let colorOption = color.children;
+let colorOptionArr = Array.from(colorOption);
+
+button.addEventListener("click",function(){
+    let colorValue = color.value;
+    colorOptionArr.forEach((item,index) => {
+        if(colorValue===item.value){
+            colorOptionArr.splice(index,1);
+            color.removeChild(color[index]);
+        }
+    });
+})
